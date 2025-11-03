04eedf4d74adf5011fd124378a5cfb9fb4933b89
paulomatheus <paulomatheus.araujo@hotmail.com>
2025-11-03 09:01:56 -0500

    Implement the GET by specific email method

--- router/friends.js
+++ router/friends.js
@@ -16,9 +16,10 @@ router.get("/",(req,res)=>{
 });
 
 // GET by specific ID request: Retrieve a single friend with email ID
-router.get("/:email",(req,res)=>{
-  // Update the code here
-  res.send("Yet to be implemented")//This line is to be replaced with actual return value
+router.get('/:email', function(req, res) {
+    // Retrieve the email parameter from the request URL and send the corresponding friend's details
+    const email = req.params.email;
+    res.send(friends[email]);
 });
 
 
